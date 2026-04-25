65a15ac0bbb982314fd84fb0c73a9cb1207efeb4
panda1331 <evelinamorova27@gmail.com>
2026-04-25 18:21:15 +0300

    feat: add AuthService

---
+++ FinanceTracker.Application/Services/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using FinanceTracker.Application.DTOs.Requests;
+using FinanceTracker.Application.DTOs.Responses;
+using FinanceTracker.Application.Interfaces.Infrastructure;
+using FinanceTracker.Application.Interfaces.Services;
+using FinanceTracker.Application.Repository;
+using FinanceTracker.Shared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Application.Services
+{
+    public class AuthService(IUserRepository userRepository, IPasswordHasher passwordHasher, ITokenGenerator tokenGenerator) : IAuthService
+    {
+        public Task<AuthResponse> LoginAsync(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthResponse> RegisterAsync(RegisterRequest request)
+        {
+            if (!CheckEmail(request.Email))
+                throw new InvalidCredentialsException()
+        }
+
+        private bool CheckEmail(string email)
+        {
+            return true;
+        }
+    }
+}
