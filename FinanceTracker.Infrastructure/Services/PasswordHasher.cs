a86d97af5cf76f11e8a02f4fdb83bf5fd8700582
panda1331 <evelinamorova27@gmail.com>
2026-04-25 23:27:58 +0300

    feat: implement PasswordHasher

---
+++ FinanceTracker.Infrastructure/Services/PasswordHasher.cs
@@ -0,0 +1,20 @@
+﻿using FinanceTracker.Application.Interfaces.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Infrastructure.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
