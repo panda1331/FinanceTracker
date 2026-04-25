3057bdbd43100054ee7a1bfd55e7d752871b8580
panda1331 <evelinamorova27@gmail.com>
2026-04-25 23:17:07 +0300

    feat: implement jwt token generator

---
+++ FinanceTracker.Infrastructure/Services/JwtSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Infrastructure.Services
+{
+    public class JwtSettings
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int ExpiryHours { get; set; }
+    }
+}
---
+++ FinanceTracker.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,41 @@
+﻿using FinanceTracker.Application.Interfaces.Infrastructure;
+using FinanceTracker.Domain.Enums;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FinanceTracker.Infrastructure.Services
+{
+    public class JwtTokenGenerator : ITokenGenerator
+    {
+        private readonly JwtSettings _settings;
+        public JwtTokenGenerator(IOptions<JwtSettings> options)
+        {
+         _settings = options.Value;   
+        }
+        public string GenerateToken(Guid userId, string email, RoleType role)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.Secret));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.Role, role.ToString())
+            };
+            var token = new JwtSecurityToken(
+                issuer: _settings.Issuer,
+                audience: _settings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(_settings.ExpiryHours),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
