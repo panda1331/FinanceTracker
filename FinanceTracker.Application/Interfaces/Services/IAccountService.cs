38c52bf2c6cf3e02256a24ece05504404d79055f
panda1331 <evelinamorova27@gmail.com>
2026-04-26 10:17:00 +0300

    feat: implement AccountService

---
+++ FinanceTracker.Application/Interfaces/Services/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using FinanceTracker.Application.DTOs.Requests;
+using FinanceTracker.Application.DTOs.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Application.Interfaces.Services
+{
+    public interface IAccountService
+    {
+        Task<AccountResponse> CreateAccountAsync(Guid userId, CreateAccountRequest request);
+        Task<List<AccountResponse>> GetAccountsByUserIdAsync(Guid userId);
+    }
+}
---
+++ FinanceTracker.Application/Services/AccountService.cs
@@ -0,0 +1,58 @@
+﻿using FinanceTracker.Application.DTOs.Requests;
+using FinanceTracker.Application.DTOs.Responses;
+using FinanceTracker.Application.Interfaces.Services;
+using FinanceTracker.Application.Repository;
+using FinanceTracker.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _repository;
+
+        public AccountService(IAccountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<AccountResponse> CreateAccountAsync(Guid userId, CreateAccountRequest request)
+        {
+            var account = new Account(request.Type, request.Name, userId);
+            await _repository.AddAsync(account);
+            await _repository.SaveChangesAsync();
+            return new AccountResponse
+            {
+                Id = account.Id,
+                Name = account.Name,
+                Type = account.Type,
+                Balance = account.Balance,
+            };
+        }
+
+        public async Task<List<AccountResponse>> GetAccountsByUserIdAsync(Guid userId)
+        {
+            var accounts = await _repository.GetByUserIdAsync(userId);
+            return MapAccountsToResponses(accounts);
+        }
+
+        private List<AccountResponse> MapAccountsToResponses(List<Account> accounts)
+        {
+            var responses = new List<AccountResponse>();
+            foreach (var account in accounts)
+            {
+                var response = new AccountResponse
+                {
+                    Id = account.Id,
+                    Name = account.Name,
+                    Type = account.Type,
+                    Balance = account.Balance,
+                };
+                responses.Add(response);
+            }
+            return responses;
+        }
+    }
+}
